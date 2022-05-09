8246fef53f3c10c156d63fe64d25c42d38b47024
Angardo18 <angelos180900@gmail.com>
2022-05-09 16:58:47 -0600

    Diente de Sierra

---
+++ Debug/main.d
@@ -0,0 +1,69 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+main.obj: D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+main.obj: D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+main.obj: D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+main.obj: D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+main.obj: D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+main.obj: D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+main.obj: D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+main.obj: D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+main.obj: D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+main.obj: D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+main.obj: D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h
+main.obj: D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h
+main.obj: D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/driverlib/debug.h
+main.obj: D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/driverlib/uart.h
+main.obj: D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/driverlib/pin_map.h
+main.obj: D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h
+main.obj: D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/inc/hw_ints.h
+main.obj: D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h
+main.obj: D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/utils/uartstdio.h
+main.obj: D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdarg.h
+
+../main.c:
+
+D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h:
+
+D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h:
+
+D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/driverlib/debug.h:
+
+D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/driverlib/uart.h:
+
+D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/driverlib/pin_map.h:
+
+D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h:
+
+D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/inc/hw_ints.h:
+
+D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h:
+
+D:/SDKs/ti/TivaWare_C_Series-2.2.0.295/utils/uartstdio.h:
+
+D:/programas/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdarg.h:
+
--- main.c
+++ main.c
@@ -3,7 +3,41 @@
 /**
  * main.c
  */
-int main(void)
-{
+#include  <stdint.h>
+#include <stdbool.h>
+
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/debug.h"
+#include "driverlib/uart.h"
+#include "driverlib/pin_map.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+//----------- utilidades -------------------
+#include "uartstdio.h"
+
+uint16_t cont = 0;
+int main(void){
+    // Frecuencia de operacion a 80 MHZ
+    SysCtlClockSet(SYSCTL_XTAL_16MHZ| SYSCTL_OSC_MAIN
+                   | SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL);
+    //---------- PUERTO UART ------------------------------
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA); //para habilitar el puertoA
+    GPIOPinConfigure(GPIO_PA0_U0RX); //configuracion del periferico al que estaran
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0); //habilitar el puerto
+    // usar el oscilador interno de 16MHz para el uart
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    // Initialize the UART for console I/O.
+    UARTStdioConfig(0, 1000000, 16000000);
+
+    while(1){
+        UARTprintf("%d\n",(cont++)%128);
+        SysCtlDelay(200000);
+    }
+
 	return 0;
 }
